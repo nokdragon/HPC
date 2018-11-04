f1a0d2c6dcacec719db9d220b3ceff9e7408da32
Cyprien <cyprien.bouni@free.fr>
2018-11-04 01:54:39 +0100

    ajout du post traitement mais avec un bug il ne marche pas quand il fait la dilatation ca annule l'érosion

--- main.c
+++ main.c
@@ -142,56 +142,156 @@ void Copy(uint8 **Mt, uint8 **It, long nrl, long nrh, long ncl, long nch){
   	}
 }
 
+
+//dilatation de carré de 3x3
+void dilatation3(uint8 **EtD, uint8 **Et, long nrl, long nrh, long ncl, long nch){
+	int i, j, k, l;
+	for(i=nrl+1; i<=nrh-1; i++) {
+	    for(j=ncl+1; j<=nch-1; j++) {
+	    	if(Et[i][j]==255){
+	    		for(k=-1;k<2;k++){
+		    		for(l=-1;l<2;l++){
+		    			EtD[i+k][j+l]=255;
+		    		}
+		    	}
+	    	}
+	    }
+  	}
+}
+
+//dilatation de carré de 5x5
+void dilatation5(uint8 **EtD, uint8 **Et, long nrl, long nrh, long ncl, long nch){
+	int i, j, k, l;
+	for(i=nrl+2; i<=nrh-2; i++) {
+	    for(j=ncl+2; j<=nch-2; j++) {
+	    	if(Et[i][j]==255){
+	    		for(k=-2;k<3;k++){
+		    		for(l=-2;l<3;l++){
+		    			EtD[i+k][j+l]=255;
+		    		}
+		    	}
+	    	}
+	    }
+  	}
+}
+
+//erosion de carré de 3x3
+void erosion3(uint8 **EtE, uint8 **Et, long nrl, long nrh, long ncl, long nch){
+	int i, j, k, l;
+	for(i=nrl+1; i<=nrh-1; i++) {
+	    for(j=ncl+1; j<=nch-1; j++) {
+	    	if(Et[i][j]==0){
+	    		for(k=-1;k<2;k++){
+		    		for(l=-1;l<2;l++){
+		    			EtE[i+k][j+l]=0;
+		    		}
+		    	}
+	    	}
+	    }
+  	}
+}
+
+//erosion de carré de 5x5
+void erosion5(uint8 **EtE, uint8 **Et, long nrl, long nrh, long ncl, long nch){
+	int i, j, k, l;
+	for(i=nrl+2; i<=nrh-2; i++) {
+	    for(j=ncl+2; j<=nch-2; j++) {
+	    	if(Et[i][j]==0){
+	    		for(k=-2;k<3;k++){
+		    		for(l=-2;l<3;l++){
+		    			EtE[i+k][j+l]=0;
+		    		}
+		    	}
+	    	}
+	    }
+  	}
+}
+
+int frame;
+
+
+//ouverture de carré 3x3
+void ouverture3(uint8 **Et, long nrl, long nrh, long ncl, long nch){
+
+	char file[255];
+
+	uint8 **tmp;
+	tmp = ui8matrix(nrl, nrh, ncl, nch);
+
+	sprintf(file,"hall_SD/ET_SD%d.pgm",frame);
+	SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,file);
+
+	erosion3(tmp, Et, nrl, nrh, ncl, nch);
+
+	sprintf(file,"hall_SD/ETe_SD%d.pgm",frame);
+	SavePGM_ui8matrix(tmp,nrl, nrh, ncl, nch,file);
+
+	dilatation3(Et, tmp,nrl, nrh, ncl, nch);
+
+	sprintf(file,"hall_SD/ETz_SD%d.pgm",frame);
+	SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,file);
+
+	free_ui8matrix(tmp, nrl, nrh, ncl, nch);
+}
+
+//ouverture de carré 5x5
+void ouverture5(uint8 **Et, long nrl, long nrh, long ncl, long nch){
+	uint8 **tmp;
+	tmp = ui8matrix(nrl, nrh, ncl, nch);
+	erosion5(tmp, Et, nrl, nrh, ncl, nch);
+	dilatation5(Et, tmp,nrl, nrh, ncl, nch);
+	free_ui8matrix(tmp, nrl, nrh, ncl, nch);
+}
+
+//fermeture de carré 3x3
+void fermeture3(uint8 **Et, long nrl, long nrh, long ncl, long nch){
+	uint8 **tmp;
+	tmp = ui8matrix(nrl, nrh, ncl, nch);
+	dilatation3(tmp, Et,nrl, nrh, ncl, nch);
+	erosion3(Et, tmp, nrl, nrh, ncl, nch);
+	free_ui8matrix(tmp, nrl, nrh, ncl, nch);
+}
+
+//fermeture de carré 5x5
+void fermeture5(uint8 **Et, long nrl, long nrh, long ncl, long nch){
+	uint8 **tmp;
+	tmp = ui8matrix(nrl, nrh, ncl, nch);
+	dilatation5(tmp, Et,nrl, nrh, ncl, nch);
+	erosion5(Et, tmp, nrl, nrh, ncl, nch);
+	free_ui8matrix(tmp, nrl, nrh, ncl, nch);
+}
+
+
+//post traitement ouverture puis fermeture
+void posTraitementOF(uint8 **Et, long nrl, long nrh, long ncl, long nch){
+
+	char file[255];
+
+	sprintf(file,"hall_SD/ET_SD%d.pgm",frame);
+	//SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,file);
+	ouverture3(Et,nrl, nrh, ncl, nch);
+	sprintf(file,"hall_SD/ETO_SD%d.pgm",frame);
+	//SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,file);
+	fermeture3(Et,nrl, nrh, ncl, nch);
+
+	ouverture5(Et,nrl, nrh, ncl, nch);
+	fermeture5(Et,nrl, nrh, ncl, nch);
+}
+
+//post traitement fermeture puis ouverture
+void posTraitementFO(uint8 **Et, long nrl, long nrh, long ncl, long nch){
+
+	fermeture3(Et,nrl, nrh, ncl, nch);
+	ouverture3(Et,nrl, nrh, ncl, nch);
+
+	fermeture5(Et,nrl, nrh, ncl, nch);
+	ouverture5(Et,nrl, nrh, ncl, nch);
+}
+
+
+
 int main(){
 	
-
-
-/*
-	long nrl, nrh, ncl, nch;
-	uint8** m_1;
-	m_1 = LoadPGM_ui8matrix("hall/hall000024.pgm", &nrl, &nrh, &ncl, &nch);
-
-	uint8** m;
-	m = LoadPGM_ui8matrix("hall/hall000025.pgm", &nrl, &nrh, &ncl, &nch);
-
-	uint8 **Ot;
-	Ot = ui8matrix(nrl, nrh, ncl, nch);
-
-	uint8 **Et;
-	Et = ui8matrix(nrl, nrh, ncl, nch);
-
-	FD(m, m_1, Ot, Et, nrl, nrh, ncl, nch);
-
-	SavePGM_ui8matrix(Ot,nrl, nrh, ncl, nch,"hall_FD/OT_FD.pgm");
-	SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,"hall_FD/ET_FD.pgm");
-
-	uint8 **Vt;
-	Vt = ui8matrix(nrl, nrh, ncl, nch);
-
-	uint8 **Vt_1;
-	Vt_1 = ui8matrix(nrl, nrh, ncl, nch);
-
-	Init_V(Vt_1, nrl, nrh, ncl, nch);
-
-	uint8 **Mt;
-	Mt = ui8matrix(nrl, nrh, ncl, nch);
-
-	uint8 **Mt_1;
-	Mt_1 = ui8matrix(nrl, nrh, ncl, nch);
-
-	Init_M(Mt_1, m_1, nrl, nrh, ncl, nch);
-
-	
-
-	SD(m, m_1, Ot, Et, Vt, Vt_1, Mt, Mt_1, nrl, nrh, ncl, nch);
-
-	SavePGM_ui8matrix(Ot,nrl, nrh, ncl, nch,"hall_SD/OT_SD.pgm");
-	SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,"hall_SD/ET_SD.pgm");
-	SavePGM_ui8matrix(Mt,nrl, nrh, ncl, nch,"hall_SD/MT_SD.pgm");
-	SavePGM_ui8matrix(Vt,nrl, nrh, ncl, nch,"hall_SD/VT_SD.pgm");
-*/
-
-
 	int i;
 	long nrl, nrh, ncl, nch;
 	uint8** m_1;
@@ -225,7 +325,7 @@ int main(){
 
 	char file[255];
 	for(i=1;i<300;i++){
-
+		frame=i;
 
 		sprintf(file,"hall/hall%6d.pgm",i);
 		char c;
@@ -250,7 +350,7 @@ int main(){
 
 		SD(m, m_1, Ot, Et, Vt, Vt_1, Mt, Mt_1, nrl, nrh, ncl, nch);
 
-
+		/*
 		sprintf(file,"hall_SD/OT_SD%d.pgm",i);
 		SavePGM_ui8matrix(Ot,nrl, nrh, ncl, nch, file);
 		sprintf(file,"hall_SD/ET_SD%d.pgm",i);
@@ -258,7 +358,15 @@ int main(){
 		sprintf(file,"hall_SD/MT_SD%d.pgm",i);
 		SavePGM_ui8matrix(Mt,nrl, nrh, ncl, nch,file);
 		sprintf(file,"hall_SD/VT_SD%d.pgm",i);
-		SavePGM_ui8matrix(Vt,nrl, nrh, ncl, nch,file);
+		SavePGM_ui8matrix(Vt,nrl, nrh, ncl, nch,file);*/
+
+		posTraitementOF(Et, nrl, nrh, ncl, nch);
+		//sprintf(file,"hall_SD/ETOF_SD%d.pgm",i);
+		//SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,file);
+
+		posTraitementFO(Et, nrl, nrh, ncl, nch);
+		//sprintf(file,"hall_SD/ETFO_SD%d.pgm",i);
+		//SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,file);
 
 		Copy(m_1, m, nrl, nrh, ncl, nch);
 		Copy(Mt_1, Mt, nrl, nrh, ncl, nch);
