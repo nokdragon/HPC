c67705ec2b688faee5f47f09777a47cc8f0a5529
BOUNI Cyprien <cyprien.bouni@free.fr>
2018-11-05 14:13:29 +0100

    ajout de Mload à la place de Load

--- main.c
+++ main.c
@@ -22,6 +22,7 @@ int main(){
 	It_1 = LoadPGM_ui8matrix("hall/hall000000.pgm", &nrl, &nrh, &ncl, &nch);
 
 	uint8** It;
+	It = ui8matrix(nrl, nrh, ncl, nch);
 
 	uint8 **Ot;
 	Ot = ui8matrix(nrl, nrh, ncl, nch);
@@ -56,26 +57,33 @@ int main(){
 
 		sprintf(file,"hall/hall%06d.pgm",i);
 
-		It = LoadPGM_ui8matrix(file, &nrl, &nrh, &ncl, &nch);
+		MLoadPGM_ui8matrix(file, nrl, nrh, ncl, nch, It);
 
 		FD(It, It_1, Ot, Et, nrl, nrh, ncl, nch);
 
 		
 		Copy(tmp, Et, nrl, nrh, ncl, nch);
 
-		posTraitementOF(Et, nrl, nrh, ncl, nch);
+		//posTraitementOF(Et, nrl, nrh, ncl, nch);
 		sprintf(file,"hall_FD/ETOF_FD%d.pgm",i);
 		SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,file);
 
 
 
-		posTraitementFO(tmp, nrl, nrh, ncl, nch);
-		sprintf(file,"hall_FD/ETFO_FD%d.pgm",i);
-		SavePGM_ui8matrix(tmp,nrl, nrh, ncl, nch,file);
+		//posTraitementFO(tmp, nrl, nrh, ncl, nch);
+		//sprintf(file,"hall_FD/ETFO_FD%d.pgm",i);
+		//SavePGM_ui8matrix(tmp,nrl, nrh, ncl, nch,file);
 
 		SD(It, It_1, Ot, Et, Vt, Vt_1, Mt, Mt_1, nrl, nrh, ncl, nch);
 
+		sprintf(file,"hall_SD/ET_SD%d.pgm",i);
+		SavePGM_ui8matrix(Et,nrl, nrh, ncl, nch,file);
+
+
+
 		Copy(tmp, Et, nrl, nrh, ncl, nch);
+
+
 
 		posTraitementOF(Et, nrl, nrh, ncl, nch);
 		sprintf(file,"hall_SD/ETOF_SD%d.pgm",i);
