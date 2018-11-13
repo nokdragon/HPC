0d3dbecc82930be574ff923cfc4374872cfeb4b9
RemiDeJesus <de.jesus.remi@gmail.com>
2018-11-13 08:35:18 +0100

    traitement du dernier octet dans simd matrix fd (je sais plus l'ordre). Toujours à tester

--- traitement_simd.c
+++ traitement_simd.c
@@ -88,6 +88,12 @@ void vuint16_fd_simd_matrix(uint8 **It, uint8 **It_1, uint8 **Ot, uint16 **Et)
 
 void vuint8_fd_simd_matrix(uint8 **It, uint8 **It_1, uint8 **Et)
 {	
+	/*
+	Description du problème : une image est composé de 83889 octets
+	Soit 5243 paquets de 128 bits + 1 octets
+	On traite d'abord un octet, puis les 5243paquet de 128 bits en simd
+	*/
+
 	vuint8 *pIt = (vuint8*) It[0];
 	vuint8 *pIt_1 = (vuint8*) It_1[0];
 	vuint8 *pEt = (vuint8 *) Et[0];
@@ -96,9 +102,15 @@ void vuint8_fd_simd_matrix(uint8 **It, uint8 **It_1, uint8 **Et)
 	{
 		pEt[i] = vuint8_fd_simd(pIt[i], pIt_1[i]);
 		//display_vuint8(res[i], "%4.0d", "res= "); puts("\n");
-	}	
+	}
 
-	//reste 1 bit
+	//l'octet 
+	uint8 *oIt = (uint8*) It[0];
+	uint8 *oIt_1 = (uint8*) It_1[0];
+	uint8 *oEt = (uint8 *) Et[0];
+	int last_indice = NB_UINT8_IMAGE - 1; 
+	oEt[last_indice] = Frame_Difference(oIt[last_indice], oIt_1[last_indice]);
+
 }
 
 /*
